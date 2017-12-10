91c436c462701604592ceccab05fff2c04fd83ab
cai <caijiaqi2017@gmail.com>
2017-12-11 01:05:06 +0800

    Machine Learning Recipes, learn from youtube videos

---
+++ Python/ML/viz.py
@@ -0,0 +1,8 @@
+from sklearn.datasets import load_iris
+iris = load_iris()
+print iris.feature_names
+print iris.target_names
+
+for i in range(len(iris.target)):
+    print "Example %d: label %s, feathers %s" % (i, iris.target[i], iris.data[i])
+
