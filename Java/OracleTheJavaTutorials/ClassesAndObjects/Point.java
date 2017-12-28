c2dffad21ad1bcb5b83973f74f5ad838bd2bbe55
caijiaqi <caijiaqi2017@gmail.com>
2017-12-28 23:59:03 +0800

    this

--- Java/OracleTheJavaTutorials/ClassesAndObjects/Point.java
+++ Java/OracleTheJavaTutorials/ClassesAndObjects/Point.java
@@ -1,9 +1,10 @@
 public class Point {
     public int x = 0;
     public int y = 0;
-    // a constructor!
-    public Point(int a, int b) {
-    x = a;
-    y = b;
+
+    //constructor
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
