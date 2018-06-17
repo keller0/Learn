6d3bb3d43319d5df663805c1d6cdf7c25c7dea17
caijiaqi <caijiaqi2017@gmail.com>
2018-06-18 03:02:32 +0800

    zigzag from leetcode

---
+++ Online_Problme_solving/ZigZag.go
@@ -0,0 +1,47 @@
+// ZigZag Conversion
+// https://leetcode.com/problems/zigzag-conversion/description/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	
+	fmt.Println("Results:", convert("AB", 1))
+}
+
+
+func convert(s string, numRows int) string {
+	if numRows == 1 { return s }
+    l := len(s)
+	var rowNum int
+	if  l < numRows {
+		rowNum = l
+	}else {
+		rowNum = numRows
+	}
+	currentRow := 0
+	down := false
+	ss := make([]string, rowNum)
+
+    for i:=0; i<l; i++ {
+
+        if currentRow == 0 || currentRow == rowNum -1 {
+			down = !down
+		}
+		ss[currentRow] += string(s[i])
+		if down {
+			currentRow++
+		} else {
+			currentRow--
+		}
+	}
+	
+	var ans string
+	for i:=0; i<len(ss); i++ {
+		ans += ss[i]
+	}
+    return ans
+}
