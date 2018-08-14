7805841e3b3cddc6bc7a77c72c88a892e3992e5f
caijiaqi <caijiaqi2017@gmail.com>
2018-08-14 23:59:12 +0800

    search insert position

---
+++ Online_Problme_solving/Search_Insert_Position/s.go
@@ -0,0 +1,30 @@
+package main
+
+
+func searchInsert(nums []int, target int) int {
+	l := len(nums)
+	if l == 0 {
+		return 0
+	}
+	ans := 0
+	for i,_ := range nums{
+		if target < nums[i] {
+			break
+		}
+		ans++
+	}
+	return ans
+}
+
+func main() {
+	cases := [][]int {
+		{1,2,3,4},
+		{3,6,7},
+		{1,3,5,6},
+		{},
+	}
+	for _, a := range cases{
+		print(searchInsert(a, 7))
+	}
+
+}
