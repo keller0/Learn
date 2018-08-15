0cd83f58c93eab46291ddf2b02fdbaf0cb27bdb3
caijiaqi <caijiaqi2017@gmail.com>
2018-08-15 23:18:19 +0800

    binary search is better

--- Online_Problme_solving/Search_Insert_Position/s.go
+++ Online_Problme_solving/Search_Insert_Position/s.go
@@ -1,14 +1,13 @@
 package main
 
-
 func searchInsert(nums []int, target int) int {
 	l := len(nums)
 	if l == 0 {
 		return 0
 	}
 	ans := 0
-	for i,_ := range nums{
-		if target < nums[i] {
+	for i, _ := range nums {
+		if target <= nums[i] {
 			break
 		}
 		ans++
@@ -16,14 +15,16 @@ func searchInsert(nums []int, target int) int {
 	return ans
 }
 
+// use binary search to do it O(log n)
+
 func main() {
-	cases := [][]int {
-		{1,2,3,4},
-		{3,6,7},
-		{1,3,5,6},
+	cases := [][]int{
+		{1, 2, 3, 4},
+		{3, 6, 7},
+		{1, 3, 5, 6},
 		{},
 	}
-	for _, a := range cases{
+	for _, a := range cases {
 		print(searchInsert(a, 7))
 	}
 
