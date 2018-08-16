c62e3521dfe176ae75a97fe6bdf488c17c239964
caijiaqi <caijiaqi2017@gmail.com>
2018-08-16 23:57:02 +0800

    Google Machine Learning Recipes 3 4 5

---
+++ Python/ML/Recipe4/i.py
@@ -0,0 +1,18 @@
+from sklearn import datasets
+iris = datasets.load_iris()
+
+X = iris.data
+Y = iris.target
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.5)
+
+from sklearn.neighbors import KNeighborsClassifier
+my_classifier = KNeighborsClassifier()
+
+my_classifier.fit(X_train, Y_train)
+
+predictions = my_classifier.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(Y_test, predictions)
