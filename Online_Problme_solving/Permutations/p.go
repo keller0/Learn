e2fdbff7a90ac19505ccd35808ecad10228ac824
caijiaqi <caijiaqi2017@gmail.com>
2018-08-17 23:50:50 +0800

    permutations

---
+++ Online_Problme_solving/Permutations/p.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func permute(nums []int) [][]int {
+	var helper func([]int, int)
+	res := [][]int{}
+
+	helper = func(arr []int, n int) {
+		if n == 1 {
+			tmp := make([]int, len(arr))
+			copy(tmp, arr)
+			res = append(res, tmp)
+		} else {
+			for i := 0; i < n; i++ {
+				helper(arr, n-1)
+				if n%2 == 1 {
+					tmp := arr[i]
+					arr[i] = arr[n-1]
+					arr[n-1] = tmp
+				} else {
+					tmp := arr[0]
+					arr[0] = arr[n-1]
+					arr[n-1] = tmp
+				}
+			}
+		}
+	}
+	helper(nums, len(nums))
+	return res
+}
+
+func main() {
+	c := []int{1, 2, 3, 4, 5}
+
+	for _, perm := range permute(c) {
+		fmt.Println(perm)
+	}
+}
