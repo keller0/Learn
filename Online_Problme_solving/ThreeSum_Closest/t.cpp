5ab910aeda671151cf6b3f75fdc5d122abf15960
caijiaqi <caijiaqi2017@gmail.com>
2018-08-21 00:15:28 +0800

    three sum closest

---
+++ Online_Problme_solving/ThreeSum_Closest/t.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int ans = nums[0] + nums[1] + nums[2];
+        for(int i = 0; i<nums.size()-2; i++) {
+            int j = i+1, k = nums.size()-1;
+            while(j < k) {
+                int sum = nums[i]+nums[j]+nums[k];
+                if(abs(sum - target) < abs(ans-target)) ans = sum;
+                if(sum < target)
+                    j++;
+                else
+                    k--;
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    vector<int> nums = {-1, 2, 1, -4};
+    Solution s;
+    cout<<s.threeSumClosest(nums, 1)<<endl;
+}
