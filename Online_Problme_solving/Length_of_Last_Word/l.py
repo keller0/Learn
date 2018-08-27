f10a00b97302578270005f508c8a8a497619c1d7
caijiaqi <caijiaqi2017@gmail.com>
2018-08-27 23:58:31 +0800

    length of last word

---
+++ Online_Problme_solving/Length_of_Last_Word/l.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(s.rstrip(' ').split(' ')[-1])
+
+s = Solution()
+print(s.lengthOfLastWord("asdasd asd"))
