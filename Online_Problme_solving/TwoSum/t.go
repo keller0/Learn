4ed7ad43583bd2228dec3fa2283041eaf27b105c
caijiaqi <caijiaqi2017@gmail.com>
2018-11-07 00:53:19 +0800

    two sum again

---
+++ Online_Problme_solving/TwoSum/t.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+var p = fmt.Println
+func twoSum(a []int, target int) []int {
+	i1, i2 := 0, len(a)-1
+	for i1 < i2 {
+		if a[i1] + a[i2] < target {
+			i1++
+		} else if  a[i1] + a[i2] > target {
+			i2--
+		} else {
+			return []int{i1+1, i2+1}
+		}
+	} 
+	return []int{}
+}
+func main() {
+	array := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	t := 9
+	p(twoSum(array, t))
+}
